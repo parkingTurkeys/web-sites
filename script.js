fe2601517b8cf4d48dfc083934a5d30a38ef7491
parking_turkeys <p-t@hackclub.app>
2025-06-11 17:01:09 -0400

    made some variable names more accurate

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let readable = false
 
 function generateTable(source) {
-    //X AND Y ARE ONE MORE THAN THE INDEX OF THE LAST ITEM
-    let x = source.length
-    let y = 0
+    //rows AND columns ARE ONE MORE THAN THE INDEX OF THE LAST ITEM
+    let rows = source.length
+    let columns = 0
     //begin the table
     let table = "<table>"
-    for (i = 0; i < x; i++) {
-        if (source[i].length > y) {
-            y = source[i].length
+    for (i = 0; i < rows; i++) {
+        if (source[i].length > columns) {
+            columns = source[i].length
         }
     }
     //for the first row
-    for (n = 0; n < x; n++) {
+    for (n = 0; n < rows; n++) {
         //each individual cell
         table += "<th>" + source[n][0] + "</th>"
     }
-    for (i = 1; i < y; i++) {
+    for (i = 1; i < columns; i++) {
         //for each row after the first
         table += "<tr>"
-        for (n = 0; n < x; n++) {
+        for (n = 0; n < rows; n++) {
             //each individual cell
             if (source[n].length /*the length of each column; are we to the point of needing repeats*/ <= i) {
                 //repeat due to the fact that we have reached the repeat point
@@ -37,7 +37,7 @@ function generateTable(source) {
     return table
 }
 
-//source is an array with x amount of arrays where y is the amount of items in the largest array; x is the width of the table and y is the height 
+//source is an array with rows amount of arrays where columns is the amount of items in the largest array; rows is the width of the table and columns is the height 
 
 function toggleReadability() {
     if (readable) {
